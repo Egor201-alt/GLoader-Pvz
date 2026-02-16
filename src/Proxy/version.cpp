9be52c782f417db394ffa8c5379bccc96b63660c
Egor201 <Egorsimikin934@gmail.com>
2026-02-16 18:29:48 +0000

    upda

--- src/Proxy/version.cpp
+++ src/Proxy/version.cpp
@@ -1,29 +1,37 @@
 #include <windows.h>
 
-// --- МАГИЯ ПЕРЕНАПРАВЛЕНИЯ (PROXY) ---
-// Эти строчки говорят компилятору: "Не делай функции сам. Просто скажи игре, 
-// что они лежат в C:\Windows\System32\version.dll"
-#pragma comment(linker, "/export:GetFileVersionInfoA=C:\\Windows\\System32\\version.GetFileVersionInfoA")
-#pragma comment(linker, "/export:GetFileVersionInfoSizeA=C:\\Windows\\System32\\version.GetFileVersionInfoSizeA")
-#pragma comment(linker, "/export:VerQueryValueA=C:\\Windows\\System32\\version.VerQueryValueA")
+// --- ПЕРЕАДРЕСАЦИЯ ДЛЯ WINMM.DLL ---
+// Мы говорим игре: "Функции лежат в системной C:\Windows\System32\winmm.dll"
 
-// Функция загрузки нашего ядра
+#pragma comment(linker, "/export:timeBeginPeriod=C:\\Windows\\System32\\winmm.timeBeginPeriod")
+#pragma comment(linker, "/export:timeEndPeriod=C:\\Windows\\System32\\winmm.timeEndPeriod")
+#pragma comment(linker, "/export:timeGetTime=C:\\Windows\\System32\\winmm.timeGetTime")
+#pragma comment(linker, "/export:PlaySoundA=C:\\Windows\\System32\\winmm.PlaySoundA")
+#pragma comment(linker, "/export:waveOutOpen=C:\\Windows\\System32\\winmm.waveOutOpen")
+
+// Функция загрузки ядра
 void LoadCore() {
-    // Пытаемся загрузить наше ядро
+    // Пытаемся загрузить ядро
     HMODULE hCore = LoadLibraryA("PvZ_Core.dll");
-    if (!hCore) {
-        // Если ядра нет - молча ничего не делаем, игра просто запустится как обычно (ванильная)
-        // Либо можно вывести MessageBox для отладки, если хочешь.
+    
+    if (hCore) {
+        MessageBoxA(NULL, "Core Loaded Successfully!", "PvZ Mod Loader", MB_OK);
+    } else {
+        // Если ядра нет или оно сломано - покажем ошибку с кодом
+        DWORD err = GetLastError();
+        char errBuff[256];
+        wsprintfA(errBuff, "Failed to load PvZ_Core.dll\nError Code: %lu", err);
+        MessageBoxA(NULL, errBuff, "Mod Loader Error", MB_ICONERROR);
     }
 }
 
-// Точка входа в DLL
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        // ТЕСТОВОЕ ОКНО
-        MessageBoxA(NULL, "PvZ Mod Loader: Injected!", "It Works!", MB_OK);
-
         DisableThreadLibraryCalls(hModule);
+        
+        // Сразу показываем окно, что мы живы
+        // MessageBoxA(NULL, "Winmm Proxy Attached!", "Debug", MB_OK);
+        
         LoadCore();
     }
     return TRUE;
